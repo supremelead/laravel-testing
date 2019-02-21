90ae053c8155dc1e36803f2100c1bbfcb794dc46
Rustem <ryabushevrustem@gmail.com>
2019-02-21 18:10:51 +0600

    Create vues

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 128);
+            $table->string('email', 128)->unique();
+            $table->string('password', 128);
             $table->rememberToken();
             $table->timestamps();
         });
